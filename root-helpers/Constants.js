7b0a675fcd6e212225a62d8715d8a7c335448501
nnhndrdsvntyn <jarkiezzzzonmc@gmail.com>
2025-12-22 15:50:24 -0500

    Add files via upload

---
+++ root-helpers/Constants.js
@@ -0,0 +1,22 @@
+const Constants = {
+    SERVER: {
+        TICK_RATE: 20 // in ticks per second
+    },
+    PLAYERS: {
+        SPEED: 200, // in units per second
+        CHAT_DURATION: 10, // in seconds
+        CHAT_MAX_LENGTH: 50
+    },
+    DECORATIONS: {
+        'rock': {
+            size: 150,
+            color: '#4f4e4e' // gray-ish color
+        }
+    },
+    MAP: {
+        WIDTH: 10000, // in units
+        HEIGHT: 10000 // in units
+    }
+}
+
+export default Constants;
---
+++ root-helpers/Decoration.js
@@ -0,0 +1,15 @@
+// -= DECORATION CLASS -= \\
+class Decoration {
+    constructor(id, x, y, type) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+        this.type = type;
+        /*
+        Types:
+        1. Rock
+        */
+    }
+}
+
+export default Decoration;
