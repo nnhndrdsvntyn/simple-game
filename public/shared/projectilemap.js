654dfea751c0c64fe03ef5dfa1218fbee50576dd
Jesly Seide <jarkiezzzzonmc@gmail.com>
2025-12-25 23:19:59 -0500

    minor

--- public/shared/projectilemap.js
+++ public/shared/projectilemap.js
@@ -3,5 +3,5 @@ projectileMap.set('bullet', {
     type: 'bullet',
     imgSrc: './images/bullet.png',
     radius: 10,
-    imgDimensions: { width: 10 * 4, height: 10 * 2 }
+    imgDimensions: { width: 10 * 2, height: 10 * 2 }
 });
